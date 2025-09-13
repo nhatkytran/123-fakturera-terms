142244d17c13f80b66322528542a3a9874b9ee25
kytran-saritasa <ky.tran@saritasa.com>
2025-09-13 14:37:41 +0700

    Improve reset css and implement background image

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
+import styles from './App.module.css';
+
 /** App component. */
 export default function App() {
   return (
-    <div>
-      <h1>Terms</h1>
-    </div>
+    <main className={styles.main}>
+      <div className={styles.backgroundContainer}>
+        <img
+          className={styles.backgroundImage}
+          src="https://storage.123fakturera.se/public/wallpapers/sverige43.jpg"
+          alt="Nature background"
+        />
+      </div>
+      <section className={styles.navigationContainer}>
+        <nav className={styles.navigation}>
+          <div>Logo</div>
+          <div>Menu</div>
+        </nav>
+      </section>
+      <div style={{ height: '2000px' }}></div>
+    </main>
   );
 }
