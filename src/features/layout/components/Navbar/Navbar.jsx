2dfaf626c3f99655d1536bfaff41ec808f05129d
kytran-saritasa <ky.tran@saritasa.com>
2025-09-13 18:33:59 +0700

    Implement navigation for large desktop

--- src/features/layout/components/Navbar/Navbar.jsx
+++ src/features/layout/components/Navbar/Navbar.jsx
@@ -1,4 +1,6 @@
-import { HOME_URL } from '@/features/layout/data';
+import clsx from 'clsx';
+
+import { ROOT_URL, NAVIGATION_LINKS } from '@/features/layout/data';
 import styles from './Navbar.module.css';
 
 /** Navbar component. */
@@ -6,14 +8,38 @@ export default function Navbar() {
   return (
     <section className={styles.navigationContainer}>
       <nav className={styles.navigation}>
-        <a href={HOME_URL}>
+        <a href={ROOT_URL}>
           <img
             className={styles.logoImage}
             src="https://storage.123fakturera.se/public/icons/diamond.png"
             alt="Diamond logo"
           />
         </a>
-        <div>Menu</div>
+        <div className={styles.actionsContainer}>
+          <ul className={styles.navigationList}>
+            {NAVIGATION_LINKS.map((link, index) => (
+              <li key={link.label}>
+                <a
+                  className={clsx(
+                    styles.navigationLink,
+                    index === NAVIGATION_LINKS.length - 1 && styles.navigationLinkLast,
+                  )}
+                  href={link.href}
+                >
+                  <p>{link.label}</p>
+                </a>
+              </li>
+            ))}
+          </ul>
+          <div className={styles.languageContainer}>
+            <span className={styles.language}>English</span>
+            <img
+              className={styles.languageIcon}
+              src="https://storage.123fakturere.no/public/flags/GB.png"
+              alt="GB flag icon"
+            />
+          </div>
+        </div>
       </nav>
     </section>
   );
--- src/features/layout/data/index.js
+++ src/features/layout/data/index.js
@@ -1,2 +1,11 @@
-/** Home URL */
-export const HOME_URL = '/';
+/** Root URL */
+export const ROOT_URL = '/';
+
+/** Navigation links */
+export const NAVIGATION_LINKS = [
+  { label: 'Home', href: 'https://www.123fakturera.se/index.html' },
+  { label: 'Order', href: 'https://www.123fakturera.se/bestall.html' },
+  { label: 'Our Customers', href: 'https://www.123fakturera.se/kunder.html' },
+  { label: 'About us', href: 'https://www.123fakturera.se/omoss.html' },
+  { label: 'Contact Us', href: 'https://www.123fakturera.se/kontaktaoss.html' },
+];
